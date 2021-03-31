b4fd336117577970b23d8285596e5787687eefa5
yamaryo765 <77099054+yamaryo765@users.noreply.github.com>
2021-03-31 20:44:40 +0900

    記事投稿画面編集

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,19 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    Article.create(article_params)
+  end
+
+
+  private
+  def article_params
+    params.require(:tweet).permit(:title, :image, :text)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery
+//= require jquery_ujs
